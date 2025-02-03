3aa590d8841160d9821b8210e428f9096a9521cf
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-02-04 06:07:51 +0630

    update

--- spring-module-1-template/getting-start-security-java-config/src/main/java/com/jdc/spring/demo/WebSecurityConfiguration.java
+++ spring-module-1-template/getting-start-security-java-config/src/main/java/com/jdc/spring/demo/WebSecurityConfiguration.java
@@ -1,7 +1,41 @@
 package com.jdc.spring.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration {
+	
+	@Bean
+	SecurityFilterChain homeFilter(HttpSecurity http) throws Exception {
+		http.securityMatcher("/")
+		     .authorizeHttpRequests(request -> {
+			     request.anyRequest().permitAll();
+		     });
+		return http.build();
+		
+	}
+	
+	@Bean
+	SecurityFilterChain resourceFilter(HttpSecurity http) throws Exception {
+		http.securityMatcher("/resources")
+		     .authorizeHttpRequests(request -> {
+			     request.anyRequest().permitAll();
+		     });
+		return http.build();
+		
+	}
+	
+	@Bean
+	SecurityFilterChain httpFilter(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(request -> {
+			     request.anyRequest().permitAll();
+		     });
+		return http.build();
+		
+	}
 }
