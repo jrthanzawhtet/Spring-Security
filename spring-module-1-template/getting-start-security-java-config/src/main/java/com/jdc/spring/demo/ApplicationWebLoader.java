7bb0b91c8b12b5f3ae7028b52f74642937a61889
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-02-04 07:31:16 +0630

    security update

---
+++ spring-module-1-template/getting-start-security-java-config/src/main/java/com/jdc/spring/demo/ApplicationWebLoader.java
@@ -0,0 +1,25 @@
+package com.jdc.spring.demo;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationWebLoader extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
